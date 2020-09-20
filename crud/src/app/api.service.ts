5e2dd6614d7c2b12da8c076d43a5e10e7f6e7188
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-20 18:54:01 +0530

    Complete CRUD API

---
+++ crud/src/app/api.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  baseUrl = 'http://127.0.0.1:8000';
+  httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private http: HttpClient) {}
+
+  getAllMovies(): Observable<any> {
+    return this.http.get(this.baseUrl + '/movies/', {
+      headers: this.httpHeaders,
+    });
+  }
+
+  getOneMovie(id): Observable<any> {
+    return this.http.get(this.baseUrl + '/movies/' + id + '/', {
+      headers: this.httpHeaders,
+    });
+  }
+
+  updateMovie(movie): Observable<any> {
+    const body = { title: movie.title, desc: movie.desc, year: movie.year };
+    return this.http.put(this.baseUrl + '/movies/' + movie.id + '/', body, {
+      headers: this.httpHeaders,
+    });
+  }
+
+  createMovie(movie): Observable<any> {
+    const body = { title: movie.title, desc: movie.desc, year: movie.year };
+    return this.http.post(this.baseUrl + '/movies/', body, {
+      headers: this.httpHeaders,
+    });
+  }
+
+  deleteMovie(id): Observable<any> {
+    return this.http.delete(this.baseUrl + '/movies/' + id + '/', {
+      headers: this.httpHeaders,
+    });
+  }
+}
